c98c83f0a3d4407e42b03efe25c6ec9b4624e102
tech4GT <varun.gupta1798@gmail.com>
2018-08-08 17:20:45 +0530

    add message encoding
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- Module.js
+++ Module.js
@@ -10,13 +10,44 @@ module.exports = function Steganography(options, UI) {
         var step = this;
         if (options.process == "encode") {
             input.getPixels(input.src, (err, pixels) => {
-                //encode
-            })
+                // encode length
+                let lengthBits = getBitsArray(message.length);
+                for (let i = 0; i < 8; i++) {
+                    let pixel = pixels.get(0, i, 0);
+                    pixel = pixel << 1;
+                    pixel = pixel & lengthBits[i];
+                    pixels.set(0, i, 0, pixel);
+                }
+                // encode data
+                for (let i = 0, j = pixels.shape[0]; i < message.length; i++) {
+                    let location = {}, charCode = message.charCodeAt();
+                    let charBits = getBitsArray(charCode - 97);
+                    for (let k of arr) {
+                        location.x = j / pixel.shape[0];
+                        location.y = j % pixels.shape[0];
+                        let originalValue = pixels.get(location.x, location.y);
+                        pixels.set(location.x, location.y, 0, originalValue & k);
+                        j++;
+                    }
+                }
+            });
         } else {
             input.getPixels(input.src, (err, pixels) => {
                 //decode
             })
         }
+
+        function getBitsArray(number) {
+            let arr = [];
+            for (let i = 0; i < 8; i++)
+                arr.push(getBitAtPos(number, i));
+            return arr;
+        }
+
+        function getBitAtPos(number, position) {
+            return (number >> position) & 1;
+        }
+
         function output(image, datauri, mimetype) {
             step.output = { src: datauri, format: mimetype };
         }
