153346cee32d6ad933bf34ae4a6dd516c1102fbf
tech4GT <varun.gupta1798@gmail.com>
2018-08-08 11:13:21 +0530

    update
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- Module.js
+++ Module.js
@@ -1,4 +1,32 @@
-module.exports = function(options, UI) {
+module.exports = function Steganography(options, UI) {
 
-    // options.message = options.message || "this is a secret message";
+    options.message = options.message || "this is a secret message";
+    options.process = options.process || "encode";
+
+    var output;
+
+    function draw(input, callback) {
+
+        var step = this;
+        if (options.process == "encode") {
+            input.getPixels(input.src, (err, pixels) => {
+                //encode
+            })
+        } else {
+            input.getPixels(input.src, (err, pixels) => {
+                //decode
+            })
+        }
+        function output(image, datauri, mimetype) {
+            step.output = { src: datauri, format: mimetype };
+        }
+
+    }
+
+    return {
+        options: options,
+        draw: draw,
+        output: output,
+        UI: UI
+    }
 }
